bdaeaaffbed7f6e267cf66309a68e1b3551f39e1
Utkarsh Gupta <34169292+Uttu316@users.noreply.github.com>
2025-02-24 22:59:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ class App extends Component {
         super(props)
         this.state = {
             renderBall: false,
-            posi : 0,
+            posi : 5,
         };
         this.renderChoice = this.renderBallOrButton.bind(this)
         this.buttonClickHandler = this.buttonClickHandler.bind(this)
